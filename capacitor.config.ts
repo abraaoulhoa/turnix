c87e2be8bc3aac2b38ed443aa9d64296dfbf38a8
Abraão Ulhoa <79339769+abraaoulhoa@users.noreply.github.com>
2026-05-29 22:45:18 -0300

    Add Android Capacitor app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.abraaoulhoa.turnix",
+  appName: "Turnix",
+  webDir: "dist",
+};
+
+export default config;
